334a3708e6e2362d62813e0461c4de79957069d5
Deepanshu Rastogi <drastogi@zalando-25706.corp.ad.zalando.net>
2017-02-20 17:55:10 +0100

    commiting to dev branch

--- src/main/java/org/zalando/pazuzu/PazuzuAppLauncher.java
+++ src/main/java/org/zalando/pazuzu/PazuzuAppLauncher.java
@@ -11,5 +11,6 @@ public class PazuzuAppLauncher {
 
     public static void main(final String[] args) {
         SpringApplication.run(PazuzuAppLauncher.class, args);
+        System.out.println("In dev env");
     }
 }
